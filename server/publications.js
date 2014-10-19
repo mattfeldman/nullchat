8adfbcb5c355f455bb49557b8d66af3110fe99f1
Matt Feldman <decaprime@gmail.com>
2014-10-18 17:00:54 -0700

    Made availableRooms publication restrictive; permissions still wide open

--- server/publications.js
+++ server/publications.js
@@ -1,7 +1,11 @@
-Meteor.publish('messages', function() {
+Meteor.publish('messages', function () {
     return Messages.find();
 });
 
-Meteor.publish('rooms',function(){
-   return Rooms.find({users:this.userId});
+Meteor.publish('currentRooms', function () {
+    //return Rooms.find({users: this.userId});
+});
+Meteor.publish('availableRooms', function () {
+    //return Rooms.find({isPrivate: false, $or: {isPrivate: true, $and: {invited: {$elemMatch: {value1: this.userId}}}}});
+    return Rooms.find({$or: [{isPrivate: false}, {isPrivate: true,invited:this.userId}]});
 });
