23b5286e235cb8b80e05c375176a9fce3b7cfb6a
Matt Feldman <decaprime@gmail.com>
2014-10-21 23:54:23 -0700

    Added basic avatar command to store image url

--- server/publications.js
+++ server/publications.js
@@ -1,11 +1,12 @@
 Meteor.publish('messages', function () {
     return Messages.find();
 });
-
 Meteor.publish('currentRooms', function () {
     //return Rooms.find({users: this.userId});
 });
 Meteor.publish('availableRooms', function () {
-    //return Rooms.find({isPrivate: false, $or: {isPrivate: true, $and: {invited: {$elemMatch: {value1: this.userId}}}}});
     return Rooms.find({$or: [{isPrivate: false}, {isPrivate: true,invited:this.userId}]});
+});
+Meteor.publish('users',function(){
+   return Meteor.users.find({},{_id:true,profile:true});
 });
