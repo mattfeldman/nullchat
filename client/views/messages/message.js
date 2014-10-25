38dbc68e6eed69b56c5b9c92af0af7233aacc1e2
Matt Feldman <decaprime@gmail.com>
2014-10-24 23:12:48 -0700

    Cached user value in message template

--- client/views/messages/message.js
+++ client/views/messages/message.js
@@ -1,18 +1,21 @@
+Template.message.created=function(){
+    this.user = Meteor.users.findOne({_id:this.data.authorId});
+};
 Template.message.helpers({
     myMessage: function(){
         return this.authorId === Meteor.userId() ? "my-message" : ""; // TODO: Be better
     },
     userName: function(){
-        user = Meteor.users.findOne({_id:this.authorId});
+        user = Template.instance().user;
         return user && user.username;
     },
     avatar: function(){
-        user = Meteor.users.findOne({_id:this.authorId});
+        user = Template.instance().user;
         if(user && user.profile && user.profile.avatar)
             return user.profile.avatar;
     },
     color: function(){
-        user = Meteor.users.findOne({_id:this.authorId});
+        user = Template.instance().user;
         if(user && user.profile && user.profile.color){
             return "border-left: 3px solid"+user.profile.color;
         }
