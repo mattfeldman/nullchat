c252a842099185045e54428e7a9cb9dbc824617d
Matt Feldman <decaprime@gmail.com>
2014-10-28 22:55:25 -0700

    Added UserStatus client configuraiton

---
+++ client/helpers/user_status.js
@@ -0,0 +1,11 @@
+Deps.autorun(function(c){
+    try{
+        UserStatus.startMonitor({
+            threshold: 30000,
+            interval: 1000,
+            idleOnBlur: false
+        });
+        c.stop();
+    }
+    catch(Exception){};
+});
