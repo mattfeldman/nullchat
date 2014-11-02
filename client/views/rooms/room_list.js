504c25e207b58899a2e244ac82eb450eb44fe68a
Matt Feldman <decaprime@gmail.com>
2014-11-01 22:47:49 -0700

    Added simple current room highlight, closing #41

--- client/views/rooms/room_list.js
+++ client/views/rooms/room_list.js
@@ -1,6 +1,9 @@
 Template.roomList.helpers({
     locked: function(){
         return this.isPrivate ? "[LOCKED] -" : "";
+    },
+    selected: function(){
+        return this._id === Session.get("currentRoom") ? "background: yellow;" : "";
     }
 });
 //Template.roomList.created = function(){
