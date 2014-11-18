1c8713c398891061b2c17ecea048d17df2bd59b4
Matt Feldman <decaprime@gmail.com>
2014-11-17 22:44:03 -0800

    Added a simple helper to scroll to a message

--- client/helpers/client.js
+++ client/helpers/client.js
@@ -1,3 +1,11 @@
 scrollChatToBottom = function() {
     $("#roomContainer").scrollTop($("#roomContainer").height());
 };
+
+scrollToMessage = function(messageId){
+    var message = $("#"+messageId);
+    if(message){
+        var container = $("#roomContainer");
+        container.scrollTop(container.scrollTop() + message.offset().top);
+    }
+};
