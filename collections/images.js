53de99e517cf623c4b85660cfd37ce9f0fa272da
Matt Feldman <decaprime@gmail.com>
2014-11-30 23:26:23 -0800

    Added collectionFS and npm identicon package to implement default avatars closing #75

---
+++ collections/images.js
@@ -0,0 +1,17 @@
+Images = new FS.Collection("images", {
+    stores: [new FS.Store.FileSystem("images", {path: "~/uploads"})]
+});
+Images.allow({
+    insert: function() {
+        return true;
+    },
+    update: function() {
+        return true;
+    },
+    remove: function() {
+        return true;
+    },
+    download: function() {
+        return true;
+    }
+});
--- collections/users.js
+++ collections/users.js
@@ -1,7 +1,35 @@
+var identicon = Meteor.npmRequire('identicon');
+
 Meteor.methods({
    'updateTypingActivity':function(room){
        //TODO: Validate room
        var timestamp = new Date();
        Meteor.users.update({_id:Meteor.userId()},{$set:{"status.lastTyping":timestamp,"status.lastActiveRoom":room}});
    }
+});
+
+Accounts.onCreateUser(function(options, user){
+    var identiconUrl =process.env.PWD+user.username+'.png';
+    // Asynchronous API (base_string, size, callback)
+    identicon.generate(user.username, 150,  Meteor.bindEnvironment(function(err, buffer) {
+        if (err) throw err;
+        var newFile = new FS.File();
+        newFile.attachData(buffer,{type:'image/png'},function(error){
+           if(error) throw error;
+            newFile.name(user.username+'.png');
+            Images.insert(newFile,function(err,fileObj){
+                Meteor.setTimeout(function(){
+                    //console.log('URL:'+Images.findOne({_id:fileObj._id}).url());
+                    Meteor.users.update({username:user.username},{$set:{'profile.avatar':'/cfs/files/images/'+fileObj._id+'/'+user.username+'.png'}});
+                },1000); //TODO: properly do this
+            });
+        });
+    }));
+
+    // We still want the default hook's 'profile' behavior.
+    if (options.profile)
+        user.profile = options.profile;
+    if(!user.profile) user.profile = {};
+    user.profile.avatar = identiconUrl;
+    return user;
 });
