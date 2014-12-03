42753e9c6b7ae967b3a421eda528b804ba094396
Matt Feldman <decaprime@gmail.com>
2014-12-03 00:09:59 -0800

    Reimplemented simple load more messages link

--- client/views/messages/message_list.js
+++ client/views/messages/message_list.js
@@ -1,9 +1,17 @@
 Template.messageList.helpers({
     messages: function() {
         return Messages.find({roomId: Session.get('currentRoom')}, {sort: {timestamp: 1}});
+    },
+    shouldShowLoadMore: function(){
+        return Session.get('messageLimit') === Messages.find({roomId: Session.get('currentRoom'),type:'plain'}).count();
     }
 });
 
 Template.messageList.created = function(){
     scrollChatToBottom();
 };
+Template.messageList.events({
+   'click .loadMore':function(event,template){
+       Session.set('messageLimit',Session.get('messageLimit')+20);
+   }
+});
