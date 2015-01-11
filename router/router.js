ed26d70c156616274c2c8cf76e8086c4ef097035
Matt Feldman <decaprime@gmail.com>
2015-01-10 22:44:07 -0800

    Add sms twilio receive hook

--- router/router.js
+++ router/router.js
@@ -59,19 +59,18 @@ Router.map(function () {
 
     this.route('user', {
         path: 'user',
-        data:function(){
-            if(this.ready())
-            {
+        data: function () {
+            if (this.ready()) {
                 return Meteor.user().profile;
             }
         }
     });
 
-    this.route('messageContext',{
-        name:'messageContext',
-        path:'message/:messageId',
-        subscriptions:function(){
-            return[
+    this.route('messageContext', {
+        name: 'messageContext',
+        path: 'message/:messageId',
+        subscriptions: function () {
+            return [
                 Meteor.subscribe('availableRooms'),
                 Meteor.subscribe('currentRooms'),
                 Meteor.subscribe('notifications'),
@@ -79,12 +78,47 @@ Router.map(function () {
                 Meteor.subscribe('memes')
             ];
         },
-        data:function(){
+        data: function () {
             return {messageId: this.params.messageId};
         }
     });
-});
 
+    this.route('messageSms', {
+        name: 'messageSms',
+        where: "server",
+        path: 'messageSms',
+        action: function () {
+            console.log(this.request.headers['x-twilio-signature']);
+            if (this.request.body) {
+                var fromNumber = this.request.body.From;
+                if (fromNumber) {
+                    var user = Meteor.users.findOne({"profile.number": fromNumber});
+                    if (user) {
+                        var message = this.request.body.Body;
+                        var roomMatch = /#([\w]+)/.exec(message);
+                        if(roomMatch && roomMatch[1]) {
+                            var roomName = roomMatch[1];
+                            var room = Rooms.findOne({name: roomName});
+                            if (room) {
+                                console.log("From " + fromNumber + " : " + message + " for " + room.name);
+                                insertMessage(user,room,{message:message,roomId:room._id},{fromMobile:true});
+                            }
+                            else {
+                                console.log("From " + fromNumber + " : " + message + " can't find room: "+ roomName);
+                            }
+                        }
+                        else {
+                            console.log("From " + fromNumber + " : " + message + " no room listed.");
+                        }
+                    }
+                    else {
+                        console.log("No user registered with " + fromNumber);
+                    }
+                }
+            }
+        }
+    });
+});
 
 Router.onBeforeAction(function (pause) {
     if (!Meteor.userId()) {
@@ -93,4 +127,4 @@ Router.onBeforeAction(function (pause) {
     else {
         this.next();
     }
-});
+}, {except: ['messageSms']});
