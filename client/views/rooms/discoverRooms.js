5cd9e76e76e06d9117596561348391ce1b14b78b
Matt Feldman <decaprime@gmail.com>
2015-01-30 21:03:40 -0800

    Actually show the unread badge for room invitations

--- client/views/rooms/discoverRooms.js
+++ client/views/rooms/discoverRooms.js
@@ -1,25 +1,29 @@
 Template.discoverRooms.helpers({
-   unreadCountBadge:function(){
-       var count = RoomInvitations.find({invitedUser:Meteor.userId(),active:true}).count();
-       if(count){
-           return "data-unread="+count;
-       }
-   }
+    unreadCountBadge: function () {
+        return RoomInvitations.find({invitedUser: Meteor.userId(), active: true}).count();
+    },
+    hasUnreadClass: function(){
+        var count = RoomInvitations.find({invitedUser: Meteor.userId(), active: true}).count();
+        if(count && count > 0){
+            return "room-invitation-unread";
+        }
+    }
 });
 
 Template.discoverRooms.events({
-    'click #discoverRooms':function(event,template){
+    'click #discoverRooms': function (event, template) {
         AntiModals.overlay("rooms");
     }
 });
 
-Template.discoverRooms.created = function(){
+Template.discoverRooms.created = function () {
     var instance = this;
     instance.roomInvitationsSub = Meteor.subscribe("roomInvitations");
 };
 
-Template.discoverRooms.destroyed = function(){
-    if(instance.roomInvitationsSub){
+Template.discoverRooms.destroyed = function () {
+    var instance = this;
+    if (instance.roomInvitationsSub) {
         instance.roomInvitationsSub.stop();
     }
 };
