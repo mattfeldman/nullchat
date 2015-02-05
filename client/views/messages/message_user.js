57127dc11284bc833241f7a034c200a69a6f7e4f
Matt Feldman <decaprime@gmail.com>
2015-02-04 20:52:47 -0800

    Default avatar coloring

--- client/views/messages/message_user.js
+++ client/views/messages/message_user.js
@@ -9,5 +9,11 @@ Template.messageUser.helpers({
     'avatar': function () {
         var user = Meteor.users.findOne({_id:this.toString()},{fields:{"profile.avatar":1}});
         return (user && user.profile && user.profile.avatar) || "/images/logo64.png";
+    },
+    'avatarBackgroundStyle': function(){
+        var user = Meteor.users.findOne({_id:this.toString()},{fields:{"profile.color":1}});
+        if(user && user.profile && user.profile.color){
+            return 'background-color: '+user.profile.color+';"';
+        }
     }
 });
