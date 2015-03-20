ab6d47757e5f15860a3a721f8b6019bfeabc3d24
Matt Feldman <decaprime@gmail.com>
2015-03-19 21:47:43 -0700

    Fix join room autocomplete to use event instead of callback

--- client/views/rooms/join_room.js
+++ client/views/rooms/join_room.js
@@ -1,28 +1,31 @@
 Template.joinRoom.helpers({
     settings: function () {
         return {
-            position: "top",
+            position: "bottom",
             limit: 5,
             rules: [
                 {
                     collection: Rooms,
                     field: "name",
                     template: Template.roomPill,
-                    matchAll: true,
-                    callback: function (doc, ele) {
-                        Meteor.call('joinRoom', doc._id, function (err, data) {
-                            if (!err) {
-                                setCurrentRoom(data);
-                                ele.val('');
-                                $("#message").focus();
-                            }
-                        });
-                    }
+                    matchAll: true
+
                 }
             ],
             rooms: function () {
                 return Rooms.find();
             }
         };
+    }
+});
+Template.joinRoom.events({
+    'autocompleteselect input': function (event, template, doc) {
+        Meteor.call('joinRoom', doc._id, function (err, data) {
+            if (!err) {
+                setCurrentRoom(data);
+                template.$('input').val('');
+                $("#message").focus();
+            }
+        });
     }
 });
