fb3748afa5e596d40b19fc561826addaddfd987f
Matt Feldman <decaprime@gmail.com>
2015-03-29 23:13:43 -0700

    add publish-counts and message related counts to user card

--- client/views/popups/user/userProfileCard.js
+++ client/views/popups/user/userProfileCard.js
@@ -1,3 +1,28 @@
 Template.userProfileCard.helpers({
-    'testString':function(){return Session.get('testString');}
+    'statsReady': function () {
+        var instance = Template.instance();
+        return instance.starsReceivedCountSubscription.ready() &&
+            instance.starsGivenCountSubscription.ready() &&
+            instance.messagesSentCountSubscription.ready();
+    }
 });
+
+Template.userProfileCard.created = function () {
+    if(this.data) {
+        this.starsReceivedCountSubscription = Meteor.subscribe('starsReceivedCount', this.data);
+        this.starsGivenCountSubscription = Meteor.subscribe('starsGivenCount', this.data);
+        this.messagesSentCountSubscription = Meteor.subscribe('messagesSentCount', this.data);
+    }
+};
+Template.userProfileCard.destroyed = function () {
+    if (this.starsReceivedCountSubscription) {
+        this.starsReceivedCountSubscription.stop();
+        console.log('destroyed');
+    }
+    if (this.starsGivenCountSubscription) {
+        this.starsGivenCountSubscription.stop();
+    }
+    if (this.messagesSentCountSubscription) {
+        this.messagesSentCountSubscription.stop();
+    }
+};
