ebb6b1fd95f33bff37a8885fcf663cf5c5f0ca4c
Matt Feldman <decaprime@gmail.com>
2015-04-13 21:15:18 -0700

    use callback and template.subscribe for mememodal

--- client/views/modals/meme/memeModal.js
+++ client/views/modals/meme/memeModal.js
@@ -1,18 +1,3 @@
-Template.memeModal.helpers({
-    'ready': function () {
-        return Template.instance().memesSubscription.ready();
-    }
+Template.memeModal.onCreated(function () {
+    this.subscribe('memes');
 });
-
-Template.memeModal.created = function () {
-    var instance = this;
-    instance.memesSubscription = Meteor.subscribe('memes');
-};
-
-Template.memeModal.destroyed = function () {
-    var instance = this;
-    if (instance.memesSubscription) {
-        instance.memesSubscription.stop();
-    }
-};
-
