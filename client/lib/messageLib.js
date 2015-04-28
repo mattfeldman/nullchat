f4124c8606e7e1dd8263febee9f1d91c922135c2
Matt Feldman <decaprime@gmail.com>
2015-04-28 08:05:15 -0700

    factor out common message functions

--- client/lib/messageLib.js
+++ client/lib/messageLib.js
@@ -3,8 +3,8 @@
  * @param message
  * @returns message with html room replacements
  */
-parseRoomLinks = function(message){
-    if(!message){ return message;}
+parseRoomLinks = function (message) {
+    if (!message) { return message;}
     var rooms = Rooms.find({}, {'_id': 1, 'name': 1}).fetch();
     rooms = _.sortBy(rooms, function (room) {
         return -room.name.length;
@@ -25,4 +25,47 @@ parseRoomLinks = function(message){
         }
     }
     return message;
-}
+};
+
+/**
+ * Parses @mentions in messages
+ * @param message
+ * @returns message with html name replacements
+ */
+parseNameMentions = function (message) {
+    var users = Meteor.users.find({}, {fields: {'_id': 1, 'username': 1, 'profile.color': 1}}).fetch();
+    users = _(users).sortBy(function (user) {
+        return -user.username.length;
+    }); // TODO: Not this every message
+    var loc = -1;
+    while ((loc = message.indexOf("@", loc + 1)) >= 0) {
+        for (var i = 0; i < users.length; i++) {
+            var userName = users[i].username;
+            if (message.indexOf(userName, loc) === loc + 1) {
+                var leftHalf = message.substring(0, loc);
+                var userColor = (users[i].profile && users[i].profile.color) || "black";
+                var styleString = 'style="border-bottom: 2px solid ' + userColor + ';' + 'background: ' + tinycolor(userColor).setAlpha(0.075).toRgbString() + ';"';
+                var middle = '<span class="message-user-mention" ' + styleString + ' data-userId="' + users[i]._id + '">@' + userName + '</span>';
+                var rightHalf = message.substring(loc + userName.length + 1, message.length + middle.length);
+                message = leftHalf + middle + rightHalf;
+                loc = loc + middle.length - 1;
+                break;
+            }
+        }
+    }
+    return message;
+};
+
+
+/**
+ * Determines if message contains current users username
+ * @param message
+ * @returns true if message contains current username, false otherwise
+ */
+hasUserMentions = function (message) {
+    if (!message || typeof  message !== "string") {return false;}
+    var regex = new RegExp("[@\\s]+(" + Meteor.user({}, {fields: {'username': 1}}).username + ")($|[\\s!.?]+)");
+    var regexMatch = message.match(regex);
+
+    return regexMatch && regexMatch.length > 0;
+};
