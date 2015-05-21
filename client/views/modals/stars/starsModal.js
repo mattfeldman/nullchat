87931fe986574407bc2c1877aa4ac273c37060da
Matt Feldman <decaprime@gmail.com>
2015-05-20 22:27:41 -0700

    Move stars modal to Template.subscribe (previously was an incomplete change causing an exception)

--- client/views/modals/stars/starsModal.js
+++ client/views/modals/stars/starsModal.js
@@ -1,10 +1,4 @@
 Template.starsModal.onCreated(function () {
-    this.subscribe('starredMessages');
-});
-
-Template.starsModal.helpers({
-    'ready': function () {
-        return Template.instance().starsSubscription.ready();
-    }
+   this.subscribe('starredMessages');
 });
 
