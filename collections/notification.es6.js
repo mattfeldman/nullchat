a1003f485caec77abbdf2da24c3b1de227fff37f
Matt Feldman <decaprime@gmail.com>
2015-07-04 20:33:14 -0700

    move notification collection to es6

--- collections/notification.es6.js
+++ collections/notification.es6.js
@@ -1,15 +1,15 @@
 Notifications = new Meteor.Collection('notifications'); // jshint ignore:line
 
 Meteor.methods({
-    'setSeen': function (roomId) {
+    setSeen(roomId) {
         check(roomId, String);
         Notifications.update({seen: false, userId: Meteor.userId(), roomId: roomId}, {$set: {seen: true}}, {multi: true});
     },
-    'setNotificationSeen':function(notificationId){
+    setNotificationSeen(notificationId){
         check(notificationId, String);
         Notifications.update({seen: false, userId: Meteor.userId(),_id:notificationId},{$set:{seen:true}});
     },
-    'setAllNotificationsSeen':function(){
+    setAllNotificationsSeen(){
         Notifications.update({seen: false, userId: Meteor.userId()},{$set:{seen:true}},{multi:true});
     }
 });
