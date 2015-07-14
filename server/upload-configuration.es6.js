8954958e424412ba4bfaadaca00c0c5780a63088
Matt Feldman <decaprime@gmail.com>
2015-07-13 18:41:10 -0700

    lint+es6 - upload configuration

--- server/upload-configuration.es6.js
+++ server/upload-configuration.es6.js
@@ -1,10 +1,10 @@
-Meteor.startup(function () {
+Meteor.startup(function() {
     // Windows Path Voodoo from https://github.com/tomitrescak/meteor-uploads/issues/49
-    var uploadDir = process.env.PWD ? process.env.PWD + '/uploads/' : "..\\..\\..\\..\\..\\uploads\\";
+    const uploadDir = process.env.PWD ? process.env.PWD + '/uploads/' : "..\\..\\..\\..\\..\\uploads\\";
     UploadServer.init({
         tmpDir: uploadDir + 'tmp',
         uploadDir: uploadDir,
-        checkCreateDirectories: true, //create the directories for you,
+        checkCreateDirectories: true, // create the directories
         maxFileSize: 250 * 1000 * 1000, // 250mb
         maxPostSize: 250 * 1000 * 1000, // 250mb
     });
