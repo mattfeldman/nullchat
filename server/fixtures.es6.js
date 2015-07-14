9d5c4c773b59171d1bd58384526a498813d051ae
Matt Feldman <decaprime@gmail.com>
2015-07-13 18:06:22 -0700

    fixtures => es6

---
+++ server/fixtures.es6.js
@@ -0,0 +1,12 @@
+var room =Rooms.findOne({name:"welcome"});
+if(!room) {
+    Rooms.insert({
+        name: "welcome",
+        topic: "welcome",
+        isPrivate: false,
+        ownerId: 0,
+        invited: [],
+        users: [],
+        moderators: []
+    });
+}
