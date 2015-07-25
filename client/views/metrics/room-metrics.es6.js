b51088dfa64d29f4f979b36616bfb2f1c8a1122b
Matt Feldman <decaprime@gmail.com>
2015-07-24 19:07:41 -0700

    fix lint issues/es6 metrics

--- client/views/metrics/room-metrics.es6.js
+++ client/views/metrics/room-metrics.es6.js
@@ -1,54 +1,53 @@
-Template.roomMetrics.created = function () {
-    var self = this;
-    Deps.autorun(function () {
-        var currentRoomUser = Meteor.users.findOne({_id: Meteor.userId()}, {"status.currentRoom": 1});
+Template.roomMetrics.onCreated(function() {
+    const self = this;
+    Deps.autorun(() => {
+        const currentRoomUser = Meteor.users.findOne({_id: Meteor.userId()}, {"status.currentRoom": 1});
         Session.set('currentRoom', currentRoomUser.status.currentRoom);
     });
-    Deps.autorun(function () {
-        var roomId = self.data.roomId === "current" ? Session.get('currentRoom') : self.data.roomId;
-        var room = Rooms.findOne({_id: roomId});
-        var chartData = [];
-        if (!room) {throw new Meteor.Error("Need room");}
-        _(room.users).each(function (userId) {
-            var user = Meteor.users.findOne({_id: userId}, {fields: {"username": 1, "profile.color": 1}});
-            Meteor.call('roomPunchcard', {userId: userId, roomId: roomId}, function (err, data) {
-                if (err) {throw err;}
+    Deps.autorun(() => {
+        const roomId = self.data.roomId === "current" ? Session.get('currentRoom') : self.data.roomId;
+        const room = Rooms.findOne({_id: roomId});
+        let chartData = [];
+        if (!room) { throw new Meteor.Error("Need room"); }
+        _(room.users).each(userId => {
+            const user = Meteor.users.findOne({_id: userId}, {fields: {"username": 1, "profile.color": 1}});
+            Meteor.call('roomPunchcard', {userId: userId, roomId: roomId}, (err, data) => {
+                if (err) {throw err; }
 
-                var seriesData = [];
-                for (var i = 0; i < 96; i++) {
+                let seriesData = [];
+                for (let i = 0; i < 96; i++) {
                     seriesData[i] = 0;
                 }
-                for (var i = 0; i < data.length; i++) {
-                    var result = data[i];
+                for (let i = 0; i < data.length; i++) {
+                    const result = data[i];
                     seriesData[result._id] = result.count;
                 }
 
-                var series = {name: user.username, data: seriesData};
+                const series = {name: user.username, data: seriesData};
                 if (user.profile && user.profile.color) {
                     series.color = user.profile.color;
                 }
                 chartData.push(series);
             });
         });
-        var chartInterval = Meteor.setInterval(
-            function () {
-                if (chartData.length < room.users.length) {
-                    return;
-                }
+        const chartInterval = Meteor.setInterval(() => {
+            if (chartData.length < room.users.length) {
+                return;
+            }
 
-                $("#roomMetricsChart").highcharts({
-                    chart: {
-                        plotBackgroundColor: null,
-                        plotBorderWidth: null,
-                        plotShadow: false,
-                        type: 'spline'
-                    },
-                    title: {
-                        text: "Message Time for #" + room.name
-                    },
-                    series: chartData
-                });
-                clearInterval(chartInterval);
-            }, 100);
+            $("#roomMetricsChart").highcharts({
+                chart: {
+                    plotBackgroundColor: null,
+                    plotBorderWidth: null,
+                    plotShadow: false,
+                    type: 'spline'
+                },
+                title: {
+                    text: "Message Time for #" + room.name
+                },
+                series: chartData
+            });
+            clearInterval(chartInterval);
+        }, 100);
     });
-}
+});
--- client/views/metrics/user-metrics.es6.js
+++ client/views/metrics/user-metrics.es6.js
@@ -1,20 +1,20 @@
-Template.userMetrics.created = function () {
-    var self = this;
-    Deps.autorun(function(){
-        var userId = self.data.userId === "current" ? Meteor.userId() : self.data.userId;
-        Meteor.call('punchcard', userId, function (err, results) {
-            if (err) { throw new Meteor.Error(err);}
+Template.userMetrics.onCreated(function() {
+    const self = this;
+    Deps.autorun(() => {
+        const userId = self.data.userId === "current" ? Meteor.userId() : self.data.userId;
+        Meteor.call('punchcard', userId, (err, results) => {
+            if (err) { throw new Meteor.Error(err); }
 
-            var chartData = [];
-            for (var i = 0; i < 288; i++) {
+            let chartData = [];
+            for (let i = 0; i < 288; i++) {
                 chartData[i] = 0;
             }
-            for (var i = 0; i < results.length; i++) {
-                var result = results[i];
+            for (let i = 0; i < results.length; i++) {
+                const result = results[i];
                 chartData[result._id] = result.count;
             }
 
-            var user = Meteor.users.findOne({_id: userId}, {fields: {"username": 1, "profile.color": 1}});
+            const user = Meteor.users.findOne({_id: userId}, {fields: {"username": 1, "profile.color": 1}});
             $("#userMetricsChart").highcharts({
                 chart: {
                     plotBackgroundColor: null,
@@ -29,4 +29,4 @@ Template.userMetrics.created = function () {
             });
         });
     });
-};
+});
