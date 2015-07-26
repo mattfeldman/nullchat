9587c6d45bc0de30224a287b47bc704ea752183c
Matt Feldman <decaprime@gmail.com>
2015-07-25 19:56:53 -0700

    notification settings button to icon

--- client/helpers/room-helpers.es6.js
+++ client/helpers/room-helpers.es6.js
@@ -1,6 +1,11 @@
 RoomHelpers = {
     activeRoomFromId(id) {
         return id && id === Session.get("currentRoom") ? "active" : "";
+    },
+    currentRoomName() {
+        const room = Rooms.findOne({_id: Session.get('currentRoom')}, {fields: {'name': 1}});
+        return room && room.name || "[unknown]";
     }
 };
 Template.registerHelper('activeRoomFromId', RoomHelpers.activeRoomFromId);
+Template.registerHelper('currentRoomName', RoomHelpers.currentRoomName);
