8a8699d0d29c55004ecbb7a9e1efcff536c8de48
Matt Feldman <decaprime@gmail.com>
2015-07-25 20:18:44 -0700

    add tests for currentRoomName

--- tests/jasmine/client/unit/room-helpers-spec.js
+++ tests/jasmine/client/unit/room-helpers-spec.js
@@ -12,4 +12,25 @@ describe('RoomHelpers', function () {
             test(undefined,"someid", '');
         });
     });
+    describe('currentRoomName', function() {
+        function test(room, expected) {
+            spyOn(Rooms, "findOne").and.returnValue(room);
+            expect(RoomHelpers.currentRoomName()).toBe(expected);
+        }
+        it('should return room\'s name', function() {
+            Session.set('currentRoom', 'foo');
+            test({name: '123'}, '123');
+            var arg = Rooms.findOne.calls.mostRecent().args[0];
+            expect(_.isEqual(arg, {_id: 'foo'})).toBeTruthy();
+        });
+        it('should return room\'s name', function() {
+            test({name: '123'}, '123');
+        });
+        it('should return default if room not found', function() {
+            test(undefined, '[unknown]');
+        });
+        it('should return default if room name not found', function() {
+            test({prop: '123'}, '[unknown]');
+        });
+    });
 });
