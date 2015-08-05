3dfcd168e542ce9b03390cba151e1deda7d3241b
Matt Feldman <decaprime@gmail.com>
2015-08-04 20:12:24 -0700

    move contents of room list template into sidebar

---
+++ client/views/sidebar/sidebar.es6.js
@@ -0,0 +1,22 @@
+Template.sidebar.helpers({
+    currentRooms() {
+        return Rooms.find({users: Meteor.userId(), direct: {$ne: true}});
+    },
+    opts() {
+        return {
+            group: "roomOrder",
+            store: {
+                get(sortable) {
+                    const userPreferences = Meteor.user().preferences;
+                    return userPreferences && userPreferences.roomOrder ? userPreferences.roomOrder : [];
+                },
+                set(sortable) {
+                    Meteor.call('updateRoomOrder', sortable.toArray());
+                }
+            }
+        };
+    },
+    currentDirectMessages() {
+        return Rooms.find({users: Meteor.userId(), direct: true});
+    }
+});
