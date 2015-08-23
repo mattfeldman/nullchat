77e7a2e6ca679e808886431e491c483890a63828
Matt Feldman <decaprime@gmail.com>
2015-08-23 10:40:57 -0400

    Add Alert Feedback to autoform error, #181

--- client/lib/autoform-config.es6.js
+++ client/lib/autoform-config.es6.js
@@ -1,3 +1,9 @@
 Meteor.startup(function() {
     AutoForm.setDefaultTemplate("semanticUI");
+
+    AutoForm.addHooks(null, {
+        onError(formType, error) {
+            AlertFeedback(error, null);
+        }
+    });
 });
