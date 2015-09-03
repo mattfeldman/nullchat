5e542f2431f69c527a07a3b6c0127d9118eca78d
Matt Feldman <decaprime@gmail.com>
2015-09-02 18:11:24 -0700

    fix unit tests/user mentions

--- client/lib/message-lib.es6.js
+++ client/lib/message-lib.es6.js
@@ -5,9 +5,7 @@ MessageLib = {
      * @returns message with html room replacements
      */
     parseRoomLinks(message) {
-        if (!message) {
-            return message;
-        }
+        check(message, String);
         let roomLinkedMessage = message;
         let rooms = this.getRoomNames();
         rooms = _.sortBy(rooms, room => -room.name.length); // TODO: Not this every message
@@ -16,10 +14,10 @@ MessageLib = {
             for (let i = 0; i < rooms.length; i++) {
                 const roomName = rooms[i].name;
                 const roomNameLowercase = rooms[i].name.toLowerCase();
-                if (message.toLowerCase().indexOf(roomNameLowercase, loc) === loc + 1) {
-                    const leftHalf = message.substring(0, loc);
+                if (roomLinkedMessage.toLowerCase().indexOf(roomNameLowercase, loc) === loc + 1) {
+                    const leftHalf = roomLinkedMessage.substring(0, loc);
                     const middle = `<a href="room/${rooms[i]._id}" class="roomLink" >#${roomName}</a>`;
-                    const rightHalf = message.substring(loc + roomName.length + 1, message.length + middle.length);
+                    const rightHalf = roomLinkedMessage.substring(loc + roomName.length + 1, roomLinkedMessage.length + middle.length);
                     roomLinkedMessage = leftHalf + middle + rightHalf;
                     loc = loc + middle.length - 1;
                     break;
@@ -41,23 +39,21 @@ MessageLib = {
      * @returns message with html name replacements
      */
     parseUserMentions(message) {
-        if (!message) {
-            return message;
-        }
+        check(message, String);
         let users = this.getUserNamesAndColors();
         let userLinkedMessage = message;
         users = _(users).sortBy(user => -user.username.length); // TODO: Not this every message
         let loc = -1;
-        while ((loc = message.indexOf("@", loc + 1)) >= 0) {
+        while ((loc = userLinkedMessage.indexOf("@", loc + 1)) >= 0) {
             for (let i = 0; i < users.length; i++) {
                 const userName = users[i].username;
-                if (message.toLowerCase().indexOf(userName.toLowerCase(), loc) === loc + 1) {
-                    const leftHalf = message.substring(0, loc);
+                if (userLinkedMessage.toLowerCase().indexOf(userName.toLowerCase(), loc) === loc + 1) {
+                    const leftHalf = userLinkedMessage.substring(0, loc);
                     const userColor = (users[i].profile && users[i].profile.color) || "black";
                     const userBackgroundColor = tinycolor(userColor).setAlpha(0.075).toRgbString();
                     const styleString = `style="border-bottom: 2px solid ${userColor}; background: ${userBackgroundColor};"`;
                     const middle = `<span class="message-user-mention" ${styleString} data-userId="${users[i]._id}">@${userName}</span>`;
-                    const rightHalf = message.substring(loc + userName.length + 1, message.length + middle.length);
+                    const rightHalf = userLinkedMessage.substring(loc + userName.length + 1, userLinkedMessage.length + middle.length);
                     userLinkedMessage = leftHalf + middle + rightHalf;
                     loc = loc + middle.length - 1;
                     break;
