7976b9acd903c9079da39d83d3640a270f179d86
Matt Feldman <decaprime@gmail.com>
2015-09-17 19:46:39 -0700

    remove test selector part of v2 migration :P

--- server/lib/migrations/v2.es6.js
+++ server/lib/migrations/v2.es6.js
@@ -3,7 +3,7 @@ Meteor.startup(function () {
         version: 2,
         name: 'Defaults continued messages on for all usersgi',
         up() {
-            Meteor.users.update({_id: Meteor.userId()}, {$set: {"profile.continueMessages": true}}, {multi: true});
+            Meteor.users.update({}, {$set: {"profile.continueMessages": true}}, {multi: true});
         }
     });
 });
