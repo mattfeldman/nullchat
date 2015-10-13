72e9b1126dc46d1fcef602534e89c2753b8843db
Matt Feldman <decaprime@gmail.com>
2015-10-12 17:57:40 -0700

    imply emojione through package and organize packages

--- packages/nullchat-emojione/package.js
+++ packages/nullchat-emojione/package.js
@@ -1,11 +1,12 @@
 Package.describe({
     summary: 'Contains nullchat wrapper of emojione',
-    version: '0.0.1',
+    version: '0.0.2',
     name: 'nullchat:emojione'
 });
 
 Package.onUse(function (api) {
-    api.use(['underscore','mongo']);
+    api.use(['underscore', 'mongo']);
+    api.imply('qnub:emojione');
     api.addFiles('emojis.js');
     api.addFiles('emojione-data.js', 'server');
     api.addFiles('emojione_import.js', 'server');
